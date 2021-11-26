063b05cbe7a88cec4aada1238e0bf344e01f5e20
Edanur Demiray <edademiray2@gmail.com>
2021-11-26 14:32:32 +0300

    Create React-Button-Toggle.js

---
+++ CoderbyteChallenges/React-Button-Toggle.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const Toggle = () => {
+  const [toggle, setToggle]=useState(false);
+  
+
+   const handleClick = ()=> {
+    setToggle(!toggle);
+  };
+
+  
+    return (
+      <button type="button" onClick={handleClick}>{toggle ? 'ON' : 'OF'}</button>
+    );
+  
+}
+
+ReactDOM.render(
+  <Toggle />,
+  document.getElementById('root')
+);
+
+
