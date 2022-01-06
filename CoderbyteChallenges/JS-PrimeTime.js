de973be0b2da6bdd44b11542359b43c7c15b9d84
Edanur Demiray <edademiray2@gmail.com>
2022-01-06 17:35:39 +0300

    Create JS-PrimeTime.js

---
+++ CoderbyteChallenges/JS-PrimeTime.js
@@ -0,0 +1,14 @@
+// Have the function PrimeTime(num) take the num parameter being passed and return the string true 
+//if the parameter is a prime number, 
+//otherwise return the string false
+//The range will be between 1 and 2^16.
+  
+
+function PrimeTime(num) { 
+
+  for( var i=2; i<Math.floor(num/2); i++)if(num%i===0) return "false";
+  return "true";
+}
+   
+// keep this function call here 
+PrimeTime(readline());
