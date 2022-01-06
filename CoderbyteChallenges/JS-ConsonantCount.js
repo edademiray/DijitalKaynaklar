11f31f35aa832e2d87f1f8fb68311146bfef5df6
Edanur Demiray <edademiray2@gmail.com>
2022-01-06 20:11:34 +0300

    Update JS-ConsonantCount.js

--- CoderbyteChallenges/JS-ConsonantCount.js
+++ CoderbyteChallenges/JS-ConsonantCount.js
@@ -1,9 +1,9 @@
 function ConsonantCount(str) { 
 
   let count = 0;
-  let sessiz = /^[aeiouAEIOU]$/;
+  let sesli = /^[aeiouAEIOU]$/;
   for( let i=0; i<str.length; i++){
-    if (!str[i].match(sessiz)&& str[i].toLowerCase() >= 'a' && str[i].toLowerCase() <= 'z'){
+    if (!str[i].match(sesli)&& str[i].toLowerCase() >= 'a' && str[i].toLowerCase() <= 'z'){
       count++;
     }
   }  
